b07f3a1cca87722dee555a3881ecb956f8c76722
Md-razvi <48431475+Md-razvi@users.noreply.github.com>
2026-07-20 20:46:40 +0530

    Let Us start

---
+++ src/main/java/com/hireAI/TalentForge/TalentForgeApplication.java
@@ -0,0 +1,13 @@
+package com.hireAI.TalentForge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentForgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentForgeApplication.class, args);
+	}
+
+}
