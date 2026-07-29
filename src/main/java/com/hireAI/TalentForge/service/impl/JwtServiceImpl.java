e02528572acc07d4090da8848daf2b0c85986526
Md-razvi <48431475+Md-razvi@users.noreply.github.com>
2026-07-30 02:34:05 +0530

    JWT authentication

--- src/main/java/com/hireAI/TalentForge/service/impl/JwtServiceImpl.java
+++ src/main/java/com/hireAI/TalentForge/service/impl/JwtServiceImpl.java
@@ -2,10 +2,38 @@ package com.hireAI.TalentForge.service.impl;
 
 import com.hireAI.TalentForge.entity.User;
 import com.hireAI.TalentForge.service.Interface.JwtService;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import java.util.Date;
 
+@Service
 public class JwtServiceImpl implements JwtService {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private Long jwtExpiration;
+
+
+    private  Key getSignInKey(){
+        byte[] keyBytes=Decoders.BASE64.decode(secretKey);
+        return  Keys.hmacShaKeyFor(keyBytes);
+    }
+
     @Override
-    public String generateToken(User user) {
-        return "";
+    public String generateToken(User user){
+        // creating a subject using jwt builder pattern
+        return Jwts.builder()
+                .subject(user.getEmail())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis()+jwtExpiration))
+                .signWith(getSignInKey())
+                .compact();
+
     }
 }
